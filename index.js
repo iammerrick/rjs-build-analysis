bd3f7aebf6e97a7da40be8873517bb0c968349de
Merrick Christensen <merrick.christensen@gmail.com>
2013-05-03 17:19:45 -0600

    Add duplicate checking.

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
+var _ = require('lodash');
 var parser = require('pegjs').buildParser(
   require('fs').readFileSync(__dirname+'/rjs.peg', 'utf8')
 );
 
 exports.parse = parser.parse;
+
+
+/**
+ * I'm fetching ashamed of this algorithm.
+ */
+exports.duplicates = function(text) {
+  var bundles = exports.parse(text).bundles;
+  var nodes = {};
+
+  _.each(bundles, function(bundle) {
+    _.each(bundle.children, function(child) {
+      nodes[child] = nodes[child] || [];
+      nodes[child].push(bundle.parent);
+    });
+  });
+
+  return _.reduce(nodes, function(memo, list, key) {
+    if (list.length > 1) {
+      memo[key] = list;
+    }
+    return memo;
+  }, {});
+}
--- test/rjs-build-analysis.js
+++ test/rjs-build-analysis.js
@@ -1,17 +1,20 @@
 var expect = require('chai').expect;
 var analyzer = require('../index');
-var read = require('fs').readFileSync;
+
+function fixture(name) {
+  return require('fs').readFileSync(__dirname+'/fixtures/'+name+'.txt', 'utf8');
+}
 
 describe('rjs-build-analysis', function() {
   describe('parse', function() {
     it('should parse the build output', function() {
-      var output = analyzer.parse(read(__dirname+'/fixtures/single-build.txt', 'utf8'));
+      var output = analyzer.parse(fixture('single-build'));
       
       expect(output).to.deep.equal({
         bundles: [
           {
             parent: 'ApplicationBootstrap.js', 
-            files: [
+            children: [
               'common/cookie.js',
               'application/csrf.js',
               'application/EventBus.js',
@@ -23,13 +26,13 @@ describe('rjs-build-analysis', function() {
     });
 
     it('should parse build output with multiple modules', function() {
-      var output = analyzer.parse(read(__dirname+'/fixtures/multiple-build.txt', 'utf8'));
+      var output = analyzer.parse(fixture('multiple-build'));
 
       expect(output).to.deep.equal({
         bundles: [
           {
             parent: 'ApplicationBootstrap.js', 
-            files: [
+            children: [
               'common/cookie.js',
               'application/csrf.js',
               'application/EventBus.js',
@@ -38,7 +41,7 @@ describe('rjs-build-analysis', function() {
           },
           {
             parent: 'OtherApp.js',
-            files: [
+            children: [
               'text!some-template.handlebars',
               'controller/SomeCtrl.js',
               'models/Some.js'
@@ -47,5 +50,15 @@ describe('rjs-build-analysis', function() {
         ]
       });
     });
+  });
+
+  describe('duplicates', function() {
+    it('should check for duplicates across modules', function() {
+      var output = analyzer.duplicates(fixture('duplicates-build'));
+      expect(output).to.deep.equal({
+        'common/cookie.js': ['ApplicationBootstrap.js', 'OtherApp.js'],
+        'another/duplicate.js': ['ApplicationBootstrap.js', 'OtherApp.js', 'YetOtherApp.js']
+      });
+    }); 
   });
 });
